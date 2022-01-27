2cbef8eb67750089d196d4d0e86b16e6a7f0994b
vikash <vikash@gmail.com>
2022-01-27 17:23:37 +0530

    new codes

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,23 @@
+package in.javahome.myweb.controller;
+import java.util.List;
+/*
+ * 
+ */
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int reminder(int i, int j){
+		return i%j;
+	}
+	
+}
